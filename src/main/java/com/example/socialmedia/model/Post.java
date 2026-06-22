58b1275cf9749bed8545fd3dec4482f16fbcdce1
kevine umukundwa <kevineumk250@gmail.com>
2026-06-22 23:00:52 +0200

    Post class

---
+++ src/main/java/com/example/socialmedia/model/Post.java
@@ -0,0 +1,35 @@
+package com.example.socialmedia.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "posts")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String title;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String content;
+
+    @Column(nullable = false)
+    private String visibility = "PUBLIC";
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @ManyToOne
+    @JoinColumn(name = "author_id", nullable = false)
+    private Author createdBy;
+}
